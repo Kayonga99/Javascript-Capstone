fa7759caf898e56bce08b42d6ac66c73beb2bf36
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-27 16:27:38 +0200

    Update branch.

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,13 +1,13 @@
-// const commentsUrl = `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/osAfoWotRUU7ff2xu5gl/comments?item_id=item1`
-import CountComments from './CountComments';
-
-class Comments {
+import CountComments from './countComements';
+export class Comments {
   FetchComments = (itemId) => {
+    console.log(itemId);
     fetch(
       `https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/osAfoWotRUU7ff2xu5gl/comments?item_id=${itemId}`,
     )
       .then((res) => res.json())
       .then((data) => {
+        console.log(data);
         if (data.error) return;
         const commentContainer = document.querySelector('.all-comments');
         commentContainer.innerHTML = data
@@ -22,3 +22,25 @@ class Comments {
         return CountComments(data);
       });
   };
+  PostComment = ({ commentId, name, textArea }) => {
+    console.log(commentId, name, textArea);
+    const config = {
+      method: 'POST',
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json; charset="utf-8"',
+      },
+      body: JSON.stringify({
+        item_id: commentId,
+        username: name,
+        comment: textArea,
+      }),
+    };
+
+    fetch(
+      'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/osAfoWotRUU7ff2xu5gl/comments',
+      config,
+    ).then(() => this.FetchComments(commentId));
+  };
+}
+export default Comments; 
--- src/modules/modalFunctionality.js
+++ src/modules/modalFunctionality.js
@@ -1,6 +1,8 @@
 /*eslint-disable*/
 const closeModal = document.querySelector('.close');
 const modalConatiner = document.getElementById('modals');
+import Comments from './comments'
+let NewComments = new Comments();
 
 export const modalsup = (e, foods) => {
   const { id } = e.target;
@@ -8,10 +10,13 @@ export const modalsup = (e, foods) => {
   foods.find((food) => {
     if (Number(id) === Number(food.idMeal)) {
       const img = document.getElementById('img');
+      const btn = document.querySelector('.comment-btn')
       const foodName = document.getElementById('foodType');
       img.src = `${food.strMealThumb}`;
       foodName.innerHTML = `${food.strMeal}`;
+      NewComments.FetchComments(food.idMeal)
       modalConatiner.style.display = 'flex';
+      btn.id = food.idMeal
     }
   });
 };
@@ -19,3 +24,20 @@ export const modalsup = (e, foods) => {
 closeModal.addEventListener('click', () => {
   modalConatiner.style.display = 'none';
 });
+
+const commentForm = document.getElementById('form')
+
+commentForm.addEventListener('submit', (e) => {
+  const btn = document.querySelector('.comment-btn')
+  e.preventDefault()
+  const name = commentForm.name;
+  const textArea = commentForm.comment;
+  NewComments.PostComment({ commentId: btn.id, name: name.value, textArea: textArea.value })
+  console.log();
+  name.value = '';
+  textArea.value = '';
+})
+
+
+
+
