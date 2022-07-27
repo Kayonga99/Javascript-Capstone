97749782381649befb640376b64484d033cd051f
Kayonga Chiteta <kayongachiteta01@gmail.com>
2022-07-27 22:34:20 +0200

    Fix linter errors.

--- src/___test___/countComent.test.js
+++ src/___test___/countComent.test.js
@@ -2,17 +2,17 @@
  * @jest-environment jsdom
  */
 
- import CountComments from '../modules/countComements';
+import CountComments from '../modules/countComements';
 
- document.body.innerHTML = '<span class="count-comment"></span>';
- 
- test('5', () => {
-   expect(CountComments([{}, {}, {}, {}, {}])).toBe(5);
- });
- 
- test('0', () => {
-   expect(CountComments([])).toBe(0);
- });
- test('2', () => {
-   expect(CountComments(['1', '2'])).toBe(2);
- });
+document.body.innerHTML = '<span class="count-comment"></span>';
+
+test('5', () => {
+  expect(CountComments([{}, {}, {}, {}, {}])).toBe(5);
+});
+
+test('0', () => {
+  expect(CountComments([])).toBe(0);
+});
+test('2', () => {
+  expect(CountComments(['1', '2'])).toBe(2);
+});
